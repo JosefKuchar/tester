46f1f7581f477270ae8454bafe419dbb24ec7450
Josef Kuchař <josef.kuchar267@gmail.com>
2019-10-06 12:30:03 +0200

    Updates

---
+++ precache-manifest.3d006693402b6e1acbfaea6e207b55e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf400a800018d1dffc76c5ca37ea931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "418579555a79e46cd1de",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "25cccd176b309a632b5c",
+    "url": "/static/js/2.478244ff.chunk.js"
+  },
+  {
+    "revision": "418579555a79e46cd1de",
+    "url": "/static/js/main.59a907ff.chunk.js"
+  },
+  {
+    "revision": "3c922a30b24ee1f23b7f",
+    "url": "/static/js/runtime-main.a9ce7d87.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
